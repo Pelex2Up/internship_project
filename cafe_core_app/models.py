ddf6dd855179bfad2287d971b4e6cd8caf37ad1c
aleksejpetrovec <neksomg123@gmail.com>
2023-01-07 12:35:13 +0300

    TEST

---
+++ cafe_core_app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Meal(models.Model):
+    name = models.CharField('Название блюда', max_length=25, blank=False)
+    description = models.TextField('Описание блюда')
+    price = models.IntegerField('Цена блюда')
+    size = models.IntegerField('Граммовка блюда')
+
+    class MealType(models.TextChoices):
+        HOT_MEALS = 'Горячие блюда'
+        DRINK = 'Напитки'
+        DESSERTS = 'Десерты'
+        NO_TYPE = 'Без типа'
+
+    meal_type = models.CharField(
+        max_length=20,
+        choices=MealType.choices,
+        default=MealType.NO_TYPE,
+    )
+
+
+class MealClick(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.DO_NOTHING)
+    click_date = models.DateTimeField('Дата клика', auto_now_add=True)
---
+++ cafe_core_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+app_name = 'cafe_core_app'
+
+urlpatterns = [
+    path('admin/', ),
+    path('', )
+]
